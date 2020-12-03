bfd904cb7f9bd176d0ae67bd0757210182a80c3b
Jaimie Sanita <jsanita11@gmail.com>
2020-12-02 21:16:56 -0500

    Day3 Assignment 3 Upload 3

--- module-1/03_Expressions/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/03_Expressions/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -361,16 +361,14 @@ public class Exercises {
 	 * false) → "10:00"
 	 */
 	public String alarmClock(int day, boolean vacation) {
-		boolean weekdays = (day == 1 || day == 2 || day == 3 || day == 4 || day == 5);
-		boolean weekends = (day == 6 || day == 7);
+		boolean weekdays = ( day > 0 && day <6);
 		if (weekdays && !vacation) {
 			return "7:00";
-		} else if (weekends && !vacation) {
+		} else if (!weekdays && !vacation) {
 			return "10:00";
 		} else if (weekdays && vacation) {
 			return "10:00";
 		} else {
-
 			return "off";
 		}
 	}
