b5ddaa76fee13219cdac1dbad8dfa468d92added
Jaimie Sanita <jsanita11@gmail.com>
2020-12-05 19:13:58 -0500

    Day 5 Assignment 5 Linear/Temp/Fibonacci

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,33 @@
 package com.techelevator;
-
+import java.util.Scanner;
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
-
+		System.out.println("Starting binary");
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter in a series of decimal values (separated by spaces): ");
+		String decimalValueString = userInput.nextLine();	
+		String [] decimalsAsIndividualStrings = decimalValueString.split(" "); 
+		 double[] binaryMath = new double[decimalsAsIndividualStrings.length]; //array called decimals holding individual decimals
+		 
+		 for(int i = 0; i < decimalsAsIndividualStrings.length; i++) {	//loop through individual decimals
+			 String stringToParse = decimalsAsIndividualStrings[i]; //pull out decimal from array
+			 double decimal = Double.parseDouble(stringToParse); // parse string to double
+			 //apply conversion math
+			 decimal = (decimal % 2); 
+			 decimal = decimal / 2;
+			 binaryMath[i] = decimal;
+			 
+		 }
 	}
 
 }
+//Write a command line program which prompts the user for a series of decimal integer values separated by spaces. 
+//Each decimal integer is displayed along with its equivalent binary value.
+//Please enter in a series of decimal values (separated by spaces): 460 8218 1 31313 987654321
+
+//460 in binary is 111001100
+//8218 in binary is 10000000011010
+//1 in binary is 1
+//31313 in binary is 111101001010001
+//987654321 in binary is 111010110111100110100010110001
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,12 +1,38 @@
 package com.techelevator;
-
+import java.util.Scanner;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-
+		System.out.println("Starting fibbonacci");
+		//allows for user input
+		Scanner userInput = new Scanner(System.in);
+		//displays prompt
+		System.out.print("Please enter the Fibonacci number: ");
+		//converts user input into string
+		String userString = userInput.nextLine();
+		//converts string into double, a number
+		int userFibNum = Integer.parseInt(userString); // consider sub integer.parseInt
+		
+		int finalFibNumInSequence = userFibNum;
+		int firstNumIsZero = 0;
+		int secondNumIsOne = 1;
+		System.out.print(firstNumIsZero);
+		//set for loop to one because sequence starts at 0,1; and i is less than or equal to startNum - 2 because already set 0 and 1 of sequence;
+		//++i increments first, then uses
+		//i++ uses first, then increments
+		for (int i=1; i <= finalFibNumInSequence; ++i) {
+	                int sum = firstNumIsZero + secondNumIsOne; //sum is 0+1
+	                firstNumIsZero = secondNumIsOne; //0=1
+	                secondNumIsOne = sum; //
+	                if (secondNumIsOne != userFibNum) {
+	                System.out.print(", " + sum);
+	                } else {
+	                	break;
+	                }
+		}
 	}
-
 }
+
 //The Fibonacci numbers are the integers in the following sequence:
 //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
 
