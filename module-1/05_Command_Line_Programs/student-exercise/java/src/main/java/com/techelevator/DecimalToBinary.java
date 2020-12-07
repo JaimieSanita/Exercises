e9d91009c6e635c9dc8fbebf57748ad790ad4fb0
Jaimie Sanita <jsanita11@gmail.com>
2020-12-07 10:37:03 -0500

    Day 5 Assignment 5 no change

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -9,19 +9,19 @@ public class DecimalToBinary {
 		String decimalValueString = userInput.nextLine();	
 		String [] decimalsAsIndividualStrings = decimalValueString.split(" "); 
 		 double[] decimalArray = new double[decimalsAsIndividualStrings.length]; //array called decimals holding individual decimals
-		 
-		 for(int i = 0; i < decimalArray.length; i++) {	//loop through individual decimals
+		
+		 int [] binaryArray = new int [20];
+		 for(int i = 0; i < 0; i--) {	//loop through individual decimals
 			 String stringToParse = decimalsAsIndividualStrings[i]; //pull out decimal from array
-			 int decimal = Integer.parseInt(stringToParse); // parse string to integer
-			 int [] binaryArray = new int [20];
-			 for (int j = 0; j < binaryArray.length; j++) {
-				 j = j / 2;
-				 binaryArray[i] = j % 2;
-			 System.out.print(stringToParse + " in binary is " + j + "\n");
+			 int decimal = Integer.parseInt(stringToParse);
+			 	decimal /= 2;
+			 	binaryArray[i] = decimal % 2;
+				 int binary = binaryArray[i];
+			 System.out.print(stringToParse + " in binary is " + binary + "\n");
 		 }
 	}
 	}
-}
+
 //Write a command line program which prompts the user for a series of decimal integer values separated by spaces. 
 //Each decimal integer is displayed along with its equivalent binary value.
 //Please enter in a series of decimal values (separated by spaces): 460 8218 1 31313 987654321
