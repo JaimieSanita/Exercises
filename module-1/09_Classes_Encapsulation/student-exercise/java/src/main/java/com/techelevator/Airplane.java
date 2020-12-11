2dc6474a3526aaa152592bfff7d99f9e134d6664
Jaimie Sanita <jsanita11@gmail.com>
2020-12-11 14:16:17 -0500

    Day 9 Assignment 9 4/7

--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Airplane.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Airplane.java
@@ -2,4 +2,72 @@ package com.techelevator;
 
 public class Airplane {
 
+	// instance variable
+
+	private String planeNumber;
+	private int totalFirstClassSeats;
+	private int bookedFirstClassSeats;
+	private int totalCoachSeats;
+	private int bookedCoachSeats;
+
+	// getters
+
+	public String getPlaneNumber() {
+		return planeNumber;
+	}
+
+	public int getTotalFirstClassSeats() {
+		return totalFirstClassSeats;
+	}
+
+	public int getBookedFirstClassSeats() {
+		return bookedFirstClassSeats;
+	}
+
+	public int getTotalCoachSeats() {
+		return totalCoachSeats;
+	}
+
+	public int getBookedCoachSeats() {
+		return bookedCoachSeats;
+	}
+
+	// derived getters
+
+	public int getAvailableFirstClassSeats() {
+		int firstClassSeats = 0;
+		firstClassSeats = this.totalFirstClassSeats - this.bookedFirstClassSeats;
+		return firstClassSeats;
+	}
+
+	public int getAvailableCoachSeats() {
+		int coachClassSeats = 0;
+		coachClassSeats = this.totalCoachSeats - this.bookedCoachSeats;
+		return coachClassSeats;
+	}
+
+	// constructor
+	public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats) {
+		this.planeNumber = planeNumber;
+		this.totalFirstClassSeats = totalFirstClassSeats;
+		this.totalCoachSeats = totalCoachSeats;
+	}
+	// method
+
+	public boolean reserveSeats(boolean forFirstClass, int totalNumberOfSeats) {
+		if (totalNumberOfSeats == 0) {
+			return true;
+		}
+		if (forFirstClass) {
+				if (totalNumberOfSeats <= getAvailableFirstClassSeats()) {
+				this.bookedFirstClassSeats = totalNumberOfSeats + bookedFirstClassSeats;
+				return true;
+			}
+		} else {
+				if (totalNumberOfSeats <= getAvailableCoachSeats()) {
+				this.bookedCoachSeats = totalNumberOfSeats + bookedCoachSeats;
+				return true;
+		}}
+		return false;
+	}
 }
--- module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_Encapsulation/student-exercise/java/src/main/java/com/techelevator/Television.java
@@ -2,4 +2,28 @@ package com.techelevator;
 
 public class Television {
 
+	
+	private boolean isOn;
+	private int currentChannel;
+	private int currentVolume;
+	
+	public boolean isOn() {
+		return this.isOn;
+	}
+	
+	public int getCurrentChannel() {
+		return currentChannel;
+	}
+	
+	public int getCurrentVolume () {
+		return currentVolume;
+	}
+	
+	// However, the instance variables need default values: a new TV is off by default. The channel is set to three and the volume level to two.
+	
+	public static final boolean defaultOn = false;
+	public static final int defaultChannel = 3;
+	public static final int defaultVolume = 2;
+	
+	
 }
