43c6af9e993c030b6f5dc7814d4751f5d751e3c4
Jaimie Sanita <jsanita11@gmail.com>
2021-02-24 14:32:11 -0500

    M3D13 HW half complete

--- module-3/13_Component_Communication/student-exercise/src/components/ReadingList.vue
+++ module-3/13_Component_Communication/student-exercise/src/components/ReadingList.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="book-container">
-
+  <div class="book-container" >
+    <book-card
+    v-for="bk in $store.state.books" v-bind:key="bk.isbn" v-bind:book="bk"/>
+    <h2></h2>
+    <img />
+    <h3></h3>
   </div>
 </template>
 
 <script>
-
+import BookCard from './BookCard.vue';
 export default {
-    name: 'reading-list'
-}
+  components: { BookCard },
+  name: "reading-list",
+};
+
 </script>
 
 <style>
 .book-container {
-    display:flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
 }
 </style>
