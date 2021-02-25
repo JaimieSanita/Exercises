70edcebd9007578c020ffe2d859e7d840fb39200
Jaimie Sanita <jsanita11@gmail.com>
2021-02-25 16:54:17 -0500

    M3D14 HW Clean and Complete

--- module-3/14_Vue_Routing/student-exercise/src/components/BookDetails.vue
+++ module-3/14_Vue_Routing/student-exercise/src/components/BookDetails.vue
@@ -14,10 +14,8 @@ export default {
     },
     computed: {
         book(){
-            //get the list of products out of our vuex store
             const books = this.$store.state.books;
-            //find returns first element where functoin true
-            //returns product with id that matchese the activeproductid from route params
+
             const book = books.find((b)=> {return b.isbn == this.$route.params.isbn;});
             return book;
         }
